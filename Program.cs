840b6a547b2261de03e0c7f4b7d786e8f6ddd009
Eric Shu <ericshurocks@gmail.com>
2021-09-11 16:04:41 -0700

    E

--- Program.cs
+++ Program.cs
@@ -1,3 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Random random = new();
+Console.WriteLine("Welcome to ConsoleDDR! To play, press the correct arrow key. Press any key to start, press Esc to quit at any time.");
+Console.ReadKey(true);
+if (Console.ReadKey().Key != ConsoleKey.Escape)
+{
+    while (true)
+    {
+        ConsoleKey cKey = ConsoleKey.Backspace;
+        int num = random.Next(1, 5);
+        if (num == 1)
+        {
+            Console.WriteLine("Left!");
+            cKey = ConsoleKey.LeftArrow;
+        }
+        else if (num == 2)
+        {
+            Console.WriteLine("Up!");
+            cKey = ConsoleKey.UpArrow;
+        }
+        else if (num == 3)
+        {
+            Console.WriteLine("Down!");
+            cKey = ConsoleKey.DownArrow;
+        }
+        else if (num == 4)
+        {
+            Console.WriteLine("Right!");
+            cKey = ConsoleKey.RightArrow;
+        }
+        var key = Console.ReadKey(true).Key;
+        if (key == ConsoleKey.Escape)
+        {
+            break;
+        }
+        else if (key != cKey)
+        {
+            Console.WriteLine("Oops! You messed up!");
+        }
+    }
+}
